0784814d67e376a8b172c8062f4e57a3f10aab48
“AlafateABULIMITI” <felixneoaa@gmail.com>
2020-05-10 18:59:36 +0200

    microblog v0.3

--- _flask/app/routes.py
+++ _flask/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route("/")
@@ -14,3 +15,14 @@ def index():
     The `render_template()` function calls the Jinja2 template engine that the Flask framework natively depends on. Jinja2 replaces `{{...}}` with the corresponding value in the incoming argument of the `render_template()` function block.
     """
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}"
+        )
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
