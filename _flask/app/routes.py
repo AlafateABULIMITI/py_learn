cededeb2dbb4787f5ca7dacd3f9e98dfcd79834e
“AlafateABULIMITI” <felixneoaa@gmail.com>
2020-05-10 15:12:56 +0200

    microblog 0.2

--- _flask/app/routes.py
+++ _flask/app/routes.py
@@ -1,7 +1,16 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
+    user = {"username": "Felix"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+    ]
+    """
+    The `render_template()` function calls the Jinja2 template engine that the Flask framework natively depends on. Jinja2 replaces `{{...}}` with the corresponding value in the incoming argument of the `render_template()` function block.
+    """
+    return render_template("index.html", title="Home", user=user, posts=posts)
